b5c04426f45f0c4d39df00e1ff247b1088894b6d
Thibault BERTHIER <tberthie@e3r10p10.42.fr>
2016-12-12 23:28:16 +0100

    up

--- srcs/input.c
+++ srcs/input.c
@@ -6,7 +6,7 @@
 /*   By: tberthie <tberthie@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/12/12 21:06:11 by tberthie          #+#    #+#             */
-/*   Updated: 2016/12/12 22:58:54 by tberthie         ###   ########.fr       */
+/*   Updated: 2016/12/12 23:26:08 by tberthie         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,13 +22,12 @@ void		input(t_msh *msh)
 	int		rd;
 
 	line = 0;
-	ft_printf("< {green}welcome %s{eoc} >\n", msh->user);
-	ft_printf("< {cyan}%s{eoc} > ", msh->user);
+	ft_printf("< {green}%s{eoc} > ", msh->user);
 	while ((rd = ft_gnl(0, &line)))
 	{
 		if (rd == -1 || (*line && !process(msh, line)))
 			return ;
 		free(line);
-		ft_printf("< {cyan}%s{eoc} > ", msh->user);
+		ft_printf("< {green}%s{eoc} > ", msh->user);
 	}
 }
